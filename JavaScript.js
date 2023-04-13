9c64a37c8c3b794d11d47afe997c6b8a6b412162
KaarJoseph <85071917+KaarJoseph@users.noreply.github.com>
2023-04-13 08:59:56 -0500

    PRÁCTICA JS

---
+++ JavaScript.js
@@ -0,0 +1,52 @@
+
+var i=0;
+
+var elemento = document.getElementById("titulo1")
+elemento,addEventListener("click", function(){
+   // alert("click en titulo");
+  //elemento.innerHTML = i;
+})
+
+
+ elemento,addEventListener("mouseover", function(){
+     i = i +1
+     console.log(i)
+})
+/*
+var menu = document.getElementById("menu")
+var listas=menu.getElementById("li")
+console.log(listas)*/
+
+/*var list = document.querySelector("ul li")
+console.log(list)*/
+
+var list2 = document.querySelectorAll("aside par_impar:nth-child(odd)")
+console.log(list2)
+
+
+function crearElemento() {
+    // Crear un nuevo elemento <p>
+    var nuevoParrafo = document.createElement("p");
+  
+    // Crear el contenido del nuevo elemento
+    var texto = document.createTextNode("Este es un nuevo párrafo");
+  
+    // Añadir el contenido al nuevo elemento
+    nuevoParrafo.appendChild(texto);
+  
+    // Añadir el nuevo elemento al div existente
+    var divExistente = document.getElementById("miDiv");
+    divExistente.appendChild(nuevoParrafo);
+  }
+  
+
+  var element = document.getElementById("addLink")  
+
+function addLink(){
+  console.log("add link 1")
+  var link=document.createElement("ul")
+  link.innerHTML = "<a href='https://google.com'>google</a>"
+  var ul = document.querySelector("aside ul")
+  ul.appendChild(link);
+  
+}
